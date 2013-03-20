0b0ebdfba35ce68503fd445b7793e5a6c349cce6
Tyson Tate <tyson@tysontate.com>
2013-03-19 23:53:56 -0700

    Fix route extending.

--- lib/nanoc_starter_set/rules.rb
+++ lib/nanoc_starter_set/rules.rb
@@ -1,6 +1,6 @@
 module NanocStarterSet
   module Rules
-    def self.included( base )
+    def self.extended( base )
       assets = File.expand_path( File.join( NANOC_STARTER_SET_ROOT, "rules", "assets.rb" ) )
       base.instance_eval do
         include_rules assets
